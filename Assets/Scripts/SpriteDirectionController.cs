2d65288eaceff2de95ea488e8e5d17db6bd4650d
Max-Mclelland <mclellandmax6040@gmail.com>
2026-02-23 22:05:52 +0000

    Sprites Moving
    I Made the code and made the prototype sprtie which will turn

--- Assets/Scripts/SpriteDirectionController.cs
+++ Assets/Scripts/SpriteDirectionController.cs
@@ -1,52 +1,106 @@
 using UnityEngine;
+using UnityEngine.UI;
+public class SpriteDirectionController : MonoBehaviour
+{
+    //On player input change to the correct sprite
 
-    //This code is a snippet from https://www.youtube.com/watch?v=UcYfEfJW_mk // also put this in asset declaration form
-    public class NewMonoBehaviourScript : MonoBehaviour
+    Direction firstPressed;
+    KeyCode activeKey = KeyCode.None;
+    KeyCode lastActiveKey = KeyCode.None;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite StickmanSpriteSheet_Foward; //so i can put the right sprites in the varaibles
+    [SerializeField] Sprite StickmanSpriteSheet_Right;
+    [SerializeField] Sprite StickmanSpriteSheet_Left;
+    [SerializeField] Sprite StickmanSpriteSheet_Back;
+
+    private void Awake()
     {
-    [SerializeField] float backAngle = 65f;
-    [SerializeField] float sideAngle = 155f;
-    [SerializeField] Transform mainTransform;
-    [SerializeField] Animator animator;
-    [SerializeField] SpriteRenderer spriteRenderer;
-    private void LateUpdate()
-    {
-        Vector3 camFowardVector = new Vector3(Camera.main.transform.foward.x, 0f, Camera.main.transform.foward.z)
-            Debug.DrawRay(Camera.main.transform.position, camFowardVector * 5f, Colour.Magenta);
-
-        float signedAngle = Vector3.SignedAngle(mainTransform.foward, camFowardVector, Vector3.up);
-
-        Vector2 animationDirection = new Vector2(0f, -1f);
-
-        float angle = Mathf.Abs(signedAngle);
-
-        //This changes the side angle based on what side the camera is viewing the slime from
-        if (angle < backAngle)
-        {
-            //back Animation
-            animationDirection = new Vector2(0f, -1f); 
-        }
-        else if (angle < sideAngle)
-        {
-            //side Animation
-            animationDirection = new Vector2(1f, 0f);
-
-            if (signedAngle < 0f) // will have to pass in own angle here
-            {
-                //left side
-                animationDirection = new Vector2(1f, 0f)
-            }
-            else
-            {
-                //right side
-                animationDirection = new Vector2(1f, 0f)
-            }
-        }
-        else
-        {
-            //Front Animation
-            animationDirection = new Vector2(0f, 1f);
-        }
-
-
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    void Update()   // Update the game on character sprite turns
+    {
+        if (activeKey == KeyCode.None)
+        {
+            GetInitialKeyCode();
+        }//used if you start moving
+
+        if (activeKey != KeyCode.None && Input.GetKeyUp(activeKey))
+        {
+            GetNewKeyCode();
+        } //used because keycode has just changed
+
+        if (activeKey != KeyCode.None && lastActiveKey == activeKey)    
+        {
+            UpdateSprite();
+        }//used to change sprite
+
+        lastActiveKey = activeKey;
+    }
+
+    void GetInitialKeyCode()
+    {
+        if ((Input.GetKeyDown(KeyCode.W)) || (Input.GetKeyDown(KeyCode.DownArrow)))
+        {
+            activeKey = KeyCode.W;
+        }   //Set active key to up
+        else if ((Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            activeKey = KeyCode.A;
+        }   //Set active key to left
+        else if ((Input.GetKeyDown(KeyCode.S)) || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            activeKey = KeyCode.S;
+        }   //Set active key to down
+        else if ((Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            activeKey = KeyCode.D;
+        }   //Set active key to right
+    }
+
+    void GetNewKeyCode()
+    {
+        if ((Input.GetKey(KeyCode.W)) || (Input.GetKeyDown(KeyCode.DownArrow))) 
+        {
+            activeKey = KeyCode.W;
+        }   //Set active key to up
+        else if ((Input.GetKey(KeyCode.A)) || (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            activeKey = KeyCode.A;
+        }   //Set active key to left
+        else if ((Input.GetKey(KeyCode.S)) || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            activeKey = KeyCode.S;
+        }   //Set active key to down
+        else if ((Input.GetKey(KeyCode.D)) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            activeKey = KeyCode.D;
+        }   //Set active key to right
+        else 
+        {
+            activeKey = KeyCode.None;
+        }
+    }   //used because keycode has just changed // uses get key "down" not just get key.
+
+    void UpdateSprite()     //Update Sprite is called if the sprite is to change
+    {
+        if ((activeKey == KeyCode.W) || (Input.GetKeyDown(KeyCode.DownArrow)))
+        {
+            spriteRenderer.sprite = StickmanSpriteSheet_Back;
+        }
+        else if ((activeKey == KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow)))
+        {
+            spriteRenderer.sprite = StickmanSpriteSheet_Left;
+        }
+        else if ((activeKey == KeyCode.S) || (Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            spriteRenderer.sprite = StickmanSpriteSheet_Foward;
+        }
+        else if ((activeKey == KeyCode.D) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            spriteRenderer.sprite = StickmanSpriteSheet_Right;
+        }
+    }
+
+    enum Direction { Empty, Up, Down, Left, Right };
+}
