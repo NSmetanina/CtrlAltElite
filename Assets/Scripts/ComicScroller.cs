95036f5d4b03d259e4045f1c82d5bba626590a2e
Max-Mclelland <mclellandmax6040@gmail.com>
2026-03-01 21:50:19 +0000

    comic scroller
    this will scroll the comic in a scene seperate from the game then load a new scene right now it loads the main scene and will  have a custom scroll speed

---
+++ Assets/Scripts/ComicScroller.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  //for  loading scenes
+using System.Collections; //for the Coroutine
+
+public class ComicScroller : MonoBehaviour
+{
+
+[SerializeField] private float YSpeed;    //serialized for tooling ie can be changed in the editor
+[SerializeField] private float WaitTime; 
+Vector3 direction; // to take values in and put the in a vector 3 controlling direction
+//camera scroll
+
+void Update() //call readInput every frame
+    {
+        ReadInput();
+    }
+
+ void FixedUpdate()
+    {
+        if(gameObject.transform.position.y <= -2.84)
+        {
+            StartCoroutine(EndOfComic());
+        }
+        else
+        {
+            Movement();
+        }
+    }
+
+    void Movement()     //change position based on direction from read input * set movement speed * fixed Deltatime ( so movement is consistent )
+    {
+        transform.position -= direction * Time.deltaTime; // fixed delta time is 
+    }
+
+    void ReadInput()
+    {
+        direction = new Vector3(0, YSpeed, 0);     // this vector 3 represents ( x, y, z ) with we will minus YSpeed by whatever value we make YSpeed
+    }
+
+
+    IEnumerator EndOfComic()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        SceneManager.LoadScene("Main_Scene_Blockout", LoadSceneMode.Single);
+    }
+}
