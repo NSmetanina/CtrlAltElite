dc9aabec14561953d213c7b0dad54f73d4701104
Max-Mclelland <mclellandmax6040@gmail.com>
2026-03-23 16:40:43 +0000

    More work on elevator+trigger to hack

--- Assets/Scripts/ElevatorMovement.cs
+++ Assets/Scripts/ElevatorMovement.cs
@@ -1,51 +1,40 @@
 using UnityEngine;
 
+    //is called when other script calls this script
+
 public class ElevatorMovement : MonoBehaviour
 {
 
-    //is called when other script calls this script
-  /*
-    [SerializeField] float elevator; 
-    [SerializeField] float StopPointUp;
-    [SerializeField] float StopPointBottom;
-    [SerializeField] private float YSpeed;
-    Vector3 elevator = new Vector3;
+    public float ElevatorYPosition;
+    public float TopStopPoint; // put in in inspector
+    public float BottomStopPoint;
+    [SerializeField] float MovementSpeed;
+    Vector3 direction;
     
-    void Start
+    void Update()
         {
-            direction = new Vector3(0, YSpeed, 0);
+            ElevatorYPosition = transform.position.y;
         }
 
-    void OnEnable() //call readInput every frame
-            {
-                
-                if (elevator == transform(0, StopPointBottom, 0))
-                    {
-                        
-                    }
-
-                if (elevator == transform(0, StopPointUp, 0))
-                    {
-                        GoDown
-                    }
-
-            }
-
-    void FixedUpdate()
+    void Start()
         {
-            if(gameObject.transform.position.y <= EndPoint) 
+            direction = new Vector3(0, MovementSpeed, 0);  
+        }
+
+    public void MoveDown()
+        {
+            while (ElevatorYPosition != BottomStopPoint)
             {
-                StartCoroutine(EndOfComic());
-            }
-            else
-            {
-                Movement();
+                transform.position -= direction * Time.deltaTime; 
             }
         }
-    void Movement()     //change position based on direction from read input * set movement speed * fixed Deltatime ( so movement is consistent )
+
+    public void MoveUp()
         {
-            transform.position -= direction * Time.deltaTime; // move based on direction
+            while (ElevatorYPosition != TopStopPoint)
+            {
+                transform.position += direction * Time.deltaTime;
+            } 
         }
-    */
+
 }
-
--- Assets/Scripts/HackableObject.cs
+++ Assets/Scripts/HackableObject.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 
+    //is called when other script calls this script
+
 public class HackableObject : MonoBehaviour
-{
+{   
+
+    public ElevatorMovement elevatorMovement;
+
     public void FunctionToCall()
     {
-                if (gameObject.tag.Contains("Elevator"))
-        {
-            Debug.Log("Function from ScriptA is called, I'm "+gameObject.name);
-        }
-
+        if (gameObject.tag.Contains("Elevator")) 
+            {
+                Debug.Log("Function from ScriptA is called, I'm " + gameObject.name); 
+                 
+                if (elevatorMovement.ElevatorYPosition == elevatorMovement.BottomStopPoint)
+                {
+                    Debug.Log("Elevator is moving up"); 
+                    elevatorMovement.MoveUp();
+                }
+                else if (elevatorMovement.ElevatorYPosition == elevatorMovement.TopStopPoint)
+                {
+                    Debug.Log("Elevator is moving down");
+                    elevatorMovement.MoveDown(); 
+                }
+                else 
+                {
+                    Debug.Log("Elevator was moving so cant run another move script"); 
+                }
+            }
+    
     }
-
 }
