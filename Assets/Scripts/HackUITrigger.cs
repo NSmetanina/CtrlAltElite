e72693bb59c8d3aa37d6123adc0ca2b2bb9ae45b
NC-1002 <nadia.s.chiu@gmail.com>
2026-03-23 21:02:50 +0000

    QTE WORKS
    YAYYYYYYYYYYYY

--- Assets/Scripts/HackUITrigger.cs
+++ Assets/Scripts/HackUITrigger.cs
@@ -5,16 +5,15 @@ public class HackTrigger : MonoBehaviour
     void Update()
     {
         PressKeyToSetActive();
+        return;
 
     }
     public GameObject HackMenu;
-    public Animator QteStartAnimator;
     public void PressKeyToSetActive()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             HackMenu.SetActive(true);
-            QteStartAnimator.SetBool("StartQte", true);
             Debug.Log ("Hack Menu Activated");
         }
 
