f0ee7d4bec196828617e6f55f47e31eb1c4d38b4
Max-Mclelland <mclellandmax6040@gmail.com>
2026-04-03 15:29:03 +0100

    Put elevator in a semi working state will move on for now

--- Assets/Max Scripts/HackableObject.cs
+++ Assets/Max Scripts/HackableObject.cs
@@ -1,36 +1,58 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;  //for  loading scenes
+using System.Collections; //for the Coroutine
 
     //is called when other script calls this script
 
 public class HackableObject : MonoBehaviour
 {   
 
+    int counter;
     public ElevatorMovement elevatorMovement;
-    
+    [SerializeField] private float WaitTime; 
     public Animator elevator;
+    bool isBlocked = false;
+
+    void Awake()
+        {
+            counter = 0;
+        }
+
+        
 
     public void HackBehaviour()
     {
-        if (gameObject.tag == "ElevatorGround") 
+        if (gameObject.tag == "ElevatorGround" && isBlocked == false) 
             {
-                Debug.Log("Function from ScriptA is called, I'm " + gameObject.name); 
+                isBlocked = true;
+                Debug.Log("Function is called, isBlocked is true, I'm " + gameObject.name); 
                  
-                if ( transform.position.z >= 0.94f)
+                if (counter % 2 == 0)
                 {
-                        Debug.Log("Hello"); 
-                    elevator.SetBool("DownTrigger", true);
-                    elevator.ResetTrigger("UpTrigger");
-                }
-                else if ( transform.position.z <= -0.88f)
-                {
-                        Debug.Log("Hello2"); 
+                        Debug.Log("Go Up"); 
                     elevator.SetBool("UpTrigger", true);
                     elevator.ResetTrigger("DownTrigger");
+                    counter = counter + 1;
+                    StartCoroutine(WaitOneSecond());
+                }
+                else if (counter % 2 == 1)
+                {
+                        Debug.Log("Go Down"); 
+                    elevator.SetBool("DownTrigger", true);
+                    elevator.ResetTrigger("UpTrigger");
+                    counter = counter + 1;
+                    StartCoroutine(WaitOneSecond());
                 }
                 else 
-                Debug.Log("Hello3"); 
+                Debug.Log("DIDNT WORK"); 
                 return;
     
         }
+    }
+    
+    IEnumerator WaitOneSecond()    //enumerators can wait
+    {
+        yield return new WaitForSeconds(WaitTime);  //wait
+        isBlocked = false;
     }
 }
